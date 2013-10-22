ce6af95c54cca05354111612ac391c717856d78c
Alex Tonkonozhenko <tonkonozhenko@gmail.com>
2013-10-22 22:36:55 +0300

    Changes map and highlighting

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -9,4 +9,21 @@ $(document).ready(function(){
     c0.scrollTop($(this).scrollTop());
     c0.scrollLeft($(this).scrollLeft());
   });
+
+  var map0 = $('#map0');
+  var map1 = $('#map1');
+  map0.on('scroll', function () {
+    map1.scrollTop($(this).scrollTop());
+    map1.scrollLeft($(this).scrollLeft());
+  });
+  map1.on('scroll', function () {
+    map0.scrollTop($(this).scrollTop());
+    map0.scrollLeft($(this).scrollLeft());
+  });
+
+  $('a').on('click', function() {
+    $('tr').removeClass('selected');
+    var id = $(this).attr('href');
+    $(id).addClass('selected');
+  })
 });
